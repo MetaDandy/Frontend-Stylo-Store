8c57459a3626133fdce67f44f8e1b7f25e34ae20
Joseph Benitez Arroyo <benitezarroyojoseph@gmail.com>
2024-09-22 18:53:32 -0400

    about, login contact, orders done, only front, without backend functionallity

--- src/About/Pages/About.jsx
+++ src/About/Pages/About.jsx
@@ -1,5 +1,81 @@
+import { assets } from "../../assets/assets";
+import { Title } from "../../Generals/Components";
+import { NewsletterBox } from "../../HomePage/Components";
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1="ABOUT" text2="US" />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          src={assets.about_img}
+          className="w-full md:max-w-[450px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, quia!
+            Cum delectus, quod veritatis labore in dolor quisquam, temporibus,
+            voluptates porro impedit totam ipsam laborum quo iusto dolores!
+            Repellendus, quibusdam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, iusto.
+            Ea praesentium at impedit illum eum architecto, molestiae
+            perspiciatis dignissimos quam obcaecati officia. Autem corporis,
+            culpa consequuntur ea praesentium consequatur.
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi
+            recusandae suscipit accusamus quibusdam ipsum, debitis dolor neque
+            quia eaque asperiores obcaecati odit nam eius cupiditate autem
+            dolore voluptate molestiae porro?
+          </p>
+        </div>
+      </div>
+      <div
+        className="text-xl py-4
+      "
+      >
+        <Title text1="WHY" text2="CHOOSE US" />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus, totam quod. Amet in maxime dignissimos vel
+            necessitatibus a minus autem, deserunt quod velit itaque reiciendis
+            magni voluptatibus natus, error quia?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus, totam quod. Amet in maxime dignissimos vel
+            necessitatibus a minus autem, deserunt quod velit itaque reiciendis
+            magni voluptatibus natus, error quia?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional customer service:</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Necessitatibus, totam quod. Amet in maxime dignissimos vel
+            necessitatibus a minus autem, deserunt quod velit itaque reiciendis
+            magni voluptatibus natus, error quia?
+          </p>
+        </div>
+      </div>
+      <div className="">
+        <NewsletterBox />
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Contact/Pages/Contact.jsx
+++ src/Contact/Pages/Contact.jsx
@@ -1,5 +1,42 @@
+import { assets } from "../../assets/assets";
+import { Title } from "../../Generals/Components";
+import { NewsletterBox } from "../../HomePage/Components";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="">
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title text1="CONTACT" text2="US" />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          src={assets.contact_img}
+          className="w-full md:max-w-[480px]"
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our store</p>
+          <p className="text-gray-500">
+            Ventura Mall <br /> Santa Cruz, Bolivia
+          </p>
+          <p className="text-gray-500">
+            Tel: +591 65933751 <br />
+            Email: benitezarroyojoseph@gmail.com
+          </p>
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className=" text-xl text-gray-600">
+            Learn more about out teams and jobs openings
+          </p>
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore more
+          </button>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default Contact;
